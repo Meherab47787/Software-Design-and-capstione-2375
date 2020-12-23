f9fa60e5f20316c605396016887d126b6b4b3fd4
Meherab47787 <52385927+Meherab47787@users.noreply.github.com>
2020-12-23 11:55:02 +0600

    Add files via upload

---
+++ Registration.php
@@ -0,0 +1,40 @@
+<html>
+
+<style>
+
+h1{ font-size: 40px;}
+
+</style> 
+    <head>
+        <title>
+            Team Build Registration
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/Registration.css">
+   
+        <body>
+            
+             <div style="background-color:#B0E0E6;" class="registerbox">
+             <img src="Images/avatar.png" class="avatar">
+             <br><br>
+             <h1> Registration</h1>
+             <br>
+                 <p style="font-size: 30px;">E-mail</p>
+                 <input style="font-size: 25px;" type="email" name="" placeholder="Enter E-mail">
+
+                 <p style="font-size: 30px;">Password</p>
+                 <input style="font-size: 25px;" type="password" name="" placeholder="Enter Password"> <br>
+
+                 <p style="font-size: 30px;">Re-enter Password</p>
+                 <input style="font-size: 25px;" type="password" name="" placeholder="Re-enter Password"> <br>
+                 <input style="font-size: 25px;" type="submit" name="" value="Register"><br><br><br>
+                 <a style="font-size: 20px;" href="Log in.html">Back to login</a><br>
+                 
+
+
+             </form>
+             </div>
+
+        </body>
+    </head>
+
+</html>
---
+++ login.php
@@ -0,0 +1,38 @@
+
+<html>
+
+<style>
+
+h1{ font-size: 60px;}
+
+</style> 
+    <head>
+        <title>
+            Team Build Login
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/LoginStyle.css">
+        <body>
+            
+             <div style="background-color:#B0E0E6;" class="loginbox">
+             <img src="Images/avatar.png" class="avatar">
+             <h1> Lonin Here</h1>
+             <br>
+                 <p style="font-size: 30px;">Username</p>
+                 <input style="font-size: 25px;" type="text" name="" placeholder="Enter Username">
+                 <p style="font-size: 30px;">Password</p>
+                 <input style="font-size: 25px;" type="password" name="" placeholder="Enter Password"> <br>
+                 <input style="font-size: 25px;" type="submit" name="" value="Login"><br><br><br>
+                 <a style="font-size: 20px;" href="#">Forgot Password?</a><br>
+                 <a style="font-size: 20px;" href="Registration.php">Register Now!</a>
+
+
+             </form>
+             </div>
+
+        </body>
+
+       
+
+    </head>
+
+</html>
